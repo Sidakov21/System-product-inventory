73d9b726876cc84bfdf738dbeab242de670cc96c
Sidakov21 <amirsidacov09@gmail.com>
2025-10-24 17:38:06 +0300

    Реализация добавления данных

---
+++ System-product-inventory/Pages/ProductEditWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace System_product_inventory.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductEditWindow.xaml
+    /// </summary>
+    public partial class ProductEditWindow : Window
+    {
+        public string ProductName { get; private set; }
+        public int Quantity { get; private set; }
+        public int Price { get; private set; }
+        public int CategoryId { get; private set; }
+
+        public ProductEditWindow(Product product = null)
+        {
+            InitializeComponent();
+
+            using (var db = new Entities())
+            {
+                CategoryComboBox.ItemsSource = db.Category.ToList();
+                CategoryComboBox.DisplayMemberPath = "Name";
+                CategoryComboBox.SelectedValuePath = "Id";
+            }
+
+            if (product != null)
+            {
+                NameBox.Text = product.Name;
+                QuantityBox.Text = product.Quantity.ToString();
+                PriceBox.Text = product.Price.ToString();
+                CategoryComboBox.SelectedValue = product.CategoryId;
+            }
+
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(NameBox.Text) ||
+                           CategoryComboBox.SelectedValue == null ||
+                           !int.TryParse(QuantityBox.Text, out int quantity) ||
+                           !int.TryParse(PriceBox.Text, out int price))
+            {
+                MessageBox.Show("Проверьте правильность введённых данных", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            ProductName = NameBox.Text.Trim();
+            Quantity = quantity;
+            Price = price;
+            CategoryId = (int)CategoryComboBox.SelectedValue;
+
+            DialogResult = true;
+            Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
--- System-product-inventory/Pages/ProductPage.xaml.cs
+++ System-product-inventory/Pages/ProductPage.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data;
+using Microsoft.Data.Sql;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +15,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Data;
-using Microsoft.Data.Sql;
 
 namespace System_product_inventory.Pages
 {
@@ -22,6 +23,8 @@ namespace System_product_inventory.Pages
     /// </summary>
     public partial class ProductPage : Page
     {
+        private ObservableCollection<Product> _items = new ObservableCollection<Product>();
+
         public ProductPage()
         {
             InitializeComponent();
@@ -32,21 +35,50 @@ namespace System_product_inventory.Pages
         {
             using (var db = new Entities())
             {
-                //var products = (from p in db.Product
-                //                join c in db.Category on p.CategoryId equals c.Id into pc
-                //                from c in pc.DefaultIfEmpty()
-                //                select new Product
-                //                {
-                //                    Name = p.Name,
-                //                    Category = c.Name,
-                //                    Quantity = p.Quantity,
-                //                    Price = p.Price
-                //                }).ToList();
+                var products = db.Product.Select(p => new
+                {
+                    p.Id,
+                    p.Name,
+                    CategoryName = db.Category
+                                     .Where(c => c.Id == p.CategoryId)
+                                     .Select(c => c.Name)
+                                     .FirstOrDefault() ?? "NULL",
+                    p.Quantity,
+                    p.Price
+                }).ToList();
 
                 // Привязываем к DataGrid
                 ProductsGrid.ItemsSource = products;
             }
         }
 
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ProductEditWindow();
+
+            if (window.ShowDialog() == true)
+            {
+                using (var db = new Entities())
+                {
+                    var product = new Product
+                    {
+                        Name = window.ProductName,
+                        Quantity = window.Quantity,
+                        Price = window.Price,
+                        CategoryId = window.CategoryId
+                    };
+
+                    db.Product.Add(product);
+                    db.SaveChanges();
+                }
+
+                LoadProducts();
+            }
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new ProductEditWindow();
+        }
     }
 }
